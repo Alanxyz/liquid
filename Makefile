8ca1f9a3acbda59682423fead62fbc845db90f08
Alan <24500077+Alanxyz@users.noreply.github.com>
2023-12-23 22:00:35 -0600

    Feat/thermalize (#2)
    * feat: thermodynamical systems and inital configurations of particles
    
    * feat: basic readme and makefile
    
    * refactor: forgett static
    
    * feat: exclude binary output from git tracking
    
    * feat: new compilation features for developing
    
    * feat: thermalization and energy stuff functions

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 liquid: main.c
-	gcc -std=c99 -o main main.c
+	gcc -std=c99 -o liquid main.c
 
 test:
-	gcc -std=c99 -o main main.c
-	./main
+	gcc -std=c99 -g -o liquid main.c
+	./liquid
--- main.c
+++ main.c
@@ -7,10 +7,13 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <math.h>
+#include <time.h>
 
 #ifndef M_PI
 #define M_PI (3.14159265358979323846)
 #endif
+
+#define URAND ((double)rand() / (double)RAND_MAX)
 
 typedef float** config_t;
 
@@ -26,7 +29,8 @@ typedef struct {
 static void die(char *errstr, ...);
 static config_t initialconfiguration(system_t *s);
 static system_t* newsystem(int nlateral, float fillfract);
-void printsystem(system_t *s);
+static void printsystem(system_t *s);
+static void thermalize(system_t *s, int stepsbyparticle, float acceptratio);
 
 void
 die(char *errstr, ...)
@@ -65,6 +69,17 @@ initialconfiguration(system_t *s)
 	return config;
 }
 
+void
+torustopology(system_t *s)
+{
+	for (int i=0; i < s->n; i++)
+	{
+		s->config[i][0] = fmod(fabs(s->config[i][0]), s->boxlenght);
+		s->config[i][1] = fmod(fabs(s->config[i][1]), s->boxlenght);
+		s->config[i][2] = fmod(fabs(s->config[i][2]), s->boxlenght);
+	}
+}
+
 system_t*
 newsystem(int nlateral, float fillfract)
 {
@@ -100,11 +115,135 @@ printsystem(system_t *s)
 	}
 }
 
+float
+radialpotential(float r)
+{
+	float dl = 50;
+	float dt = 1.4737;
+
+	float da = dl * pow((dl / (dl - 1)), (dl - 1));
+	float v = (da / dt) * (pow((1 / r), dl) - pow((1 / r), (dl - 1))) + 1 / dt;
+	return v;
+}
+
+float
+molecularpotential(system_t *s, int i, int j)
+{
+	float xi = fmod(fabs(s->config[i][0]), s->boxlenght);
+	float yi = fmod(fabs(s->config[i][1]), s->boxlenght);
+	float zi = fmod(fabs(s->config[i][2]), s->boxlenght);
+	float xj = fmod(fabs(s->config[j][0]), s->boxlenght);
+	float yj = fmod(fabs(s->config[j][1]), s->boxlenght);
+	float zj = fmod(fabs(s->config[j][2]), s->boxlenght);
+	float x2 = pow(fabs(xi - xj), 2);
+	float y2 = pow(fabs(yi - yj), 2);
+	float z2 = pow(fabs(zi - zj), 2);
+	float r = sqrt(x2 + y2 + z2);
+
+	if (r >= s->boxlenght / 2)
+		return 0;
+
+	return radialpotential(r);
+}
+
+float
+particleenergy(system_t *s, int i)
+{
+	float acc = 0;
+	for (int j = 0; j < s->n && j != i; j++)
+		acc += molecularpotential(s, i, j);
+
+	return acc;
+}
+
+float
+systemenergy(system_t *s)
+{
+	float acc = 0;
+	for (int i = 0; i < s->n; i++)
+		for (int j = 0; j < i; j++)
+			acc += molecularpotential(s, i, j);
+	return acc;
+}
+
+float
+trymoveaparticle(system_t *s, float *E, float *drmax, int *acc, int *att)
+{
+	int i = rand() % s->n;
+	float energy = particleenergy(s, i);
+	float ri[3] = {s->config[i][0], s->config[i][1], s->config[i][2]};
+	float dx = *drmax * (URAND - 0.5);
+	float dy = *drmax * (URAND - 0.5);
+	float dz = *drmax * (URAND - 0.5);
+	s->config[i][0] += dx;
+	s->config[i][1] += dy;
+	s->config[i][2] += dz;
+	torustopology(s);
+	
+	float energynew = particleenergy(s, i);
+	float denergy = energynew - energy;
+
+	if (expf(-denergy) > URAND)
+	{
+		(*acc)++;
+		*E += denergy; 
+	} else {
+		s->config[i][0] = ri[0];
+		s->config[i][1] = ri[1];
+		s->config[i][2] = ri[2];
+	}
+	(*att)++;
+}
+
+void
+adjustdrmax(float *drmax, int acc, int att, float acceptratio)
+{
+
+	if ((float)acc / att < acceptratio)
+		*drmax *= 0.95;
+	else
+		*drmax *= 1.05;
+}
+
+void
+thermalize(system_t *s, int stepsbyparticle, float acceptratio)
+{
+	float drmax = 0.1;
+	float E = systemenergy(s);
+	int acc = 0;
+	int att = 0;
+	int cycles = stepsbyparticle * s->n / acceptratio;
+	for (int i = 0;  i < cycles; i++)
+	{
+		trymoveaparticle(s, &E, &drmax, &acc, &att);
+		adjustdrmax(&drmax, acc, att, acceptratio);
+		printf("energy: %f\tdrmax: %f\tratio: \%f\n", E, drmax, (float)acc / att);
+	}
+}
+
+void
+savesystem(system_t *s)
+{
+	FILE *f = fopen("init.dat", "w");
+	if (!f)
+		die("Cannot open file.");
+	for (int i = 0; i < s->n; i++)
+	{
+		float x = s->config[i][0];
+		float y = s->config[i][1];
+		float z = s->config[i][2];
+		fprintf(f, "%f\t%f\t\%f\n", x, y, z);
+	}
+	fclose(f);
+}
+
 int
 main(int argc, char *argv[])
 {
-	system_t *s = newsystem(3, 0.35);
-	printsystem(s);
+	srand(time(0));
+	system_t *s = newsystem(5, 0.35);
+	thermalize(s, 5, 0.3);
+	savesystem(s);
 
 	return 0;
 }
